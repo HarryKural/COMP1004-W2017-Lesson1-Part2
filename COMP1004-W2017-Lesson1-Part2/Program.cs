bb03ed8cc456e09d53d394f6702208dd840c43ad
HarryKural <harrykural333@gmail.com>
2017-01-16 16:13:12 -0500

    Add project files.

---
+++ COMP1004-W2017-Lesson1-Part2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP1004_W2017_Lesson1_Part2
+{
+    
+
+
+    /// <summary>
+    /// Driver class for our console application
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Tom = new Person("Tom", 30);
+            Tom.SaysHello();
+
+            Console.WriteLine();
+
+            Person Jane = new Person("Jane", 26);
+            Jane.SaysHello();
+        }
+    }
+}
